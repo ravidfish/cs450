2abf2c4c44d92823db09a0cdab488c8f6d27ed71
Alex <ravidfish@gmail.com>
2015-07-14 18:37:38 -0600

    pushing out riot api code

---
+++ riot_api/src/riot_api/Riot_api.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package riot_api;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Mark
+ */
+public class Riot_api {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        for (int i = 0; i < 1000; i++) {
+            try {
+                /*
+                set variables by calling the setters
+                call matchid 's submit
+                save off the object data, prob json to csv
+                
+                setMatchId()
+                setApi_key()
+                
+                */
+                
+                
+                
+                Thread.sleep(1500);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(Riot_api.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+    
+}
